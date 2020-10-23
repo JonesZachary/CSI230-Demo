a28bd250eda3917140adc735ff745c4a50395d17
Zachary Jones <zacharyjones@pop-os.localdomain>
2020-10-22 20:00:06 -0400

    Checks if user exists, and updates password if true

--- Project_2/mail.sh
+++ Project_2/mail.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
+# Checks if the User ID is 0, if not then user is not root and exits
 if [ "$EUID" -ne 0 ]
   then echo "Must run as root, try: sudo ./mail.sh"
   exit
 fi
 
+# For all the emails in the txt file, cut out username
 for user in $(cat emails.txt | cut -d "@" -f 1);
 do
+  # Stores the username and generates a new password 
   username=$user
   password=$(openssl rand -base64 12)
 
+  # Checks if the user already exists and has a password, if so update password
+  egrep -q -i "^${username}:" /etc/passwd
+  if [ $? -eq 0 ]; then
+    echo "User already exists, changing ${username}'s password"
+  # If user DNE, creates a new user, adds them to CSI230, and sets the password
+  else
+  echo "${username} does not exist, creating a new account and new password"
   useradd -m -g CSI230 -s /bin/bash $username -p $password
-  chage -d 0 $user
+  # Next login, change password
+  chage -d 0 $username
 
-  echo "Username is: ${username}"
-  echo "Random password is: ${password}"
+  fi
+
 done
